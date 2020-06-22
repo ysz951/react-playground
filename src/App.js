00bc7f081198c1f578234f43f29079ac95bcac12
ysz951 <shengyang_zhou@baylor.edu>
2020-06-22 00:46:18 -0500

    add loading section

--- src/App.js
+++ src/App.js
@@ -28,65 +28,12 @@ const sectionsProp = [
 ]
 
 class App extends React.Component {
-  state = {
-    shoppingItems: [
-      /* put stub items in here for testing */
-      { name: 'apples', checked: false },
-      { name: 'oranges', checked: true },
-      { name: 'bread', checked: false },
-    ]
-  };
-  handleDeleteItem = (item) => {
-    const newItems = this.state.shoppingItems.filter(itm => itm !== item)
-    this.setState({
-      shoppingItems: newItems
-    })
-  }
-  handleCheckItem = (item) => {
-    const newItems = this.state.shoppingItems.map(itm => {
-      if (itm === item) {
-        itm.checked = !itm.checked
-      }
-      return itm
-    })
-    this.setState({
-      shoppingItems: newItems
-    })
-  }
 
-  handleAddItem = (itemName) => {
-    const newItems = [
-      ...this.state.shoppingItems,
-      { name: itemName, checked: false }
-    ]
-    this.setState({
-      shoppingItems: newItems
-    })
-  }
   render() {
     return (
       <div className="App">
         <DemonynApp/>
       </div>
-      // <> 
-      //   <header>
-      //     <h1>Shopping List</h1>
-      //   </header>
-      //   <main>
-      //     <section>
-      //       <AddItemForm 
-      //         onAddItem={this.handleAddItem} 
-      //       />
-      //     </section>
-      //     <section>
-      //       <ShoppingList 
-      //         items={this.state.shoppingItems}
-      //         onDeleteItem={this.handleDeleteItem}
-      //         onCheckItem={this.handleCheckItem} 
-      //       />
-      //     </section>
-      //   </main>
-      // </>
     )
   }
 }
--- src/demonymapp/demonymApp.js
+++ src/demonymapp/demonymApp.js
@@ -4,13 +4,69 @@ import './demonymApp.css'
 import Demonym from './demonym';
 import CountrySelector from './countrySelector';
 
-
 class DemonymApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+      selected: null,
+      loading: true,
+    };
+  }
+  setSelected(selected) {
+    this.setState({
+      selected
+    });
+  }
+  componentDidMount() {
+    fetch('https://country.register.gov.uk/records.json?page-size=5000')
+      .then(response => {
+        // check if response is ok
+        console.log('About to check for errors');
+        if(!response.ok) {
+          console.log('An error did occur, let\'s throw an error.');
+          throw new Error('Something went wrong'); // throw an error
+        }
+        return response; // ok, so just continue
+      })
+      .then(response => response.json())
+      .then(data => {
+        const countries = Object.keys(data)
+              .map(key => data[key].item[0]);
+        this.setState({
+          countries,
+          loading: false,
+        });
+      })
+      .catch(err => {
+        // this catch handles the error condition
+        // console.log('Handling the error here.', err);
+        this.setState({
+            error: err.message,
+            loading: false,
+        })
+      });
+  }
+
   render() {
+    const load = this.state.loading ? <div className="loading">Loading</div> : "";
+
+    const demon = this.state.selected
+          ? <Demonym name={this.state.selected['citizen-names']} country={this.state.selected.name}/>
+          : <div className="demonym_app__placeholder">Select a country above</div>;
+    const error = this.state.error
+          ? <div className="demonym_app__error">{this.state.error}</div>
+          : "";
+
     return (
       <div className="demonym_app">
-        <CountrySelector countries={[{name:"Barbados"}, {name:"Bahrain"}]}/>
-        <Demonym name="Barbadian" country="Barbados"/>
+        <CountrySelector
+          countries={this.state.countries}
+          changeHandler={selected => this.setSelected(selected)}/>
+        {demon}
+        {error}
+        {load}
       </div>
     );
   }
