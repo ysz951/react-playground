253cdf5789b267915d68ed6a16cc358f66fff55d
ysz951 <shengyang_zhou@baylor.edu>
2020-06-26 06:23:07 -0500

    initialize

---
+++ src/lang-context/AppLang.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class AppLang extends Component {
+  render() {
+    return (
+      <div className='AppLang'>
+        App context
+      </div>
+    );
+  }
+}
