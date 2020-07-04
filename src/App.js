d202ccd56f7cc08783b882f0e0c03250a37473de
ysz951 <shengyang_zhou@baylor.edu>
2020-07-04 16:31:36 -0500

    initialize

--- src/App.js
+++ src/App.js
@@ -1,17 +1,81 @@
 import React, { Component } from 'react';
-import './App.css';
+import { render } from 'react-dom';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      age: 0,
+      tooYoung: false
+    };
+    // create a ref
+    this.age = React.createRef();
+  }
 
-class App extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.age < 13) this.setState(this.age.current.focus())
+    // this.setState({
+    //   tooYoung: this.state.age < 13
+    // }, () => {
+    //   if(this.state.tooYoung) {
+    //     // reset the focus on the input
+    //     this.age.current.focus();
+    //   }
+    // });
+  }
+
+  handleChange(age) {
+    this.setState({
+      age
+    });
+  }
 
   render() {
     return (
-      <div className="App">
-        <p>123</p>
-      </div>
-    )
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <label htmlFor="age">Enter your age:
+          <span>(required)</span>
+        </label>
+        <input 
+          type="number" 
+          id="age" 
+          name="age" 
+          aria-required="true"
+          aria-describedby="ageDescription"
+          aria-label="Enter you age"
+          ref={this.age}
+          onChange={e => this.handleChange(parseInt(e.target.value), 10)}
+          aria-invalid={this.state.tooYoung}/>
+        <div id="ageDescription">You must be 13 years of age or older to use this site.</div>  
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <button type="submit">Go</button>
+      </form>
+    );
   }
 }
-
 
 export default App;
